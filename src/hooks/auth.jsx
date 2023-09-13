09d8cfd3ae6de9721fb3564c93553a119d9d6a29
eduaardofranco <eduaardo.franco@gmail.com>
2023-09-13 09:25:18 +0100

    singn in authentication

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState } from "react"
+import { api } from '../services/api'
+
+export const AuthContext = createContext({})
+
+function AuthProvider({ children }) {
+    const [ data, setData ] = useState({})
+    
+    async function signIn({ email, password }) {
+
+        try {
+            //create sesseion when login
+            const response = await api.post("/sessions", { email, password })
+            const { user, token } = response.data
+
+            //insert token type bearer of authorization in the header off all requests
+            api.defaults.headers.authorization = `Bearer ${token}`
+            setData({ user, token })
+
+        } catch(error) {
+            if(error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert('Fail to Enter')
+            }
+        }
+
+
+    }
+
+    return(
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
+        {/* children is <Routes> that are inside AuthProvider in main.js */}
+        { children }
+    </AuthContext.Provider>
+    )
+}
+
+function useAuth() {
+    const context = useContext(AuthContext)
+
+    return context
+}
+
+export { AuthProvider, useAuth }
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,10 +1,25 @@
+
+import { useAuth } from '../../hooks/auth'
+
 import { FiMail, FiLock } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import { Container, Form, Background } from './styles'
+
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { useState } from 'react'
 
 export function SignIn() {
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
+
+    const { signIn } = useAuth()
+
+    function handleSignIn() {
+        
+        signIn({ email, password })
+    }
+
     return(
         <Container>
             <Form>
@@ -15,13 +30,15 @@ export function SignIn() {
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={ e => setEmail(e.target.value)}
                 />
                 <Input
                     placeholder="Password"
                     type="password"
                     icon={FiLock}
+                    onChange={ e => setPassword(e.target.value)}
                 />
-                <Button title="Enter" />
+                <Button title="Enter" onClick={ handleSignIn } />
 
                 <Link to="/register">
                     Rgister
