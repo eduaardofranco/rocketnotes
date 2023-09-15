39df76fbce1d64249095a76c7d2e341b369ebeb7
eduaardofranco <eduaardo.franco@gmail.com>
2023-09-15 20:50:07 +0100

    signout in the details page bug

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,11 +3,18 @@ import { Container, Profile, Logout } from './styles'
 import { useAuth } from "../../hooks/auth" 
 import { api } from '../../services/api'
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import { useNavigate } from 'react-router-dom'
 
 
 
 export function Header() {
     const { signOut, user } = useAuth()
+    const navigation = useNavigate()
+
+    function handleSignOut() {
+        navigation("/")
+        signOut()
+    }
 
     //check if user has avatar, if not set placeholder avatar
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}files/${user.avatar}`: avatarPlaceholder
@@ -21,7 +28,7 @@ export function Header() {
                     <strong>{user.name}</strong>
                 </div>
             </Profile>
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine />
             </Logout>
         </Container>
